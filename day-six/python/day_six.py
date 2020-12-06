6afdf350ab22d5a0cc25761b0305f5365467c3c0
Devon T <axiomabsolute@gmail.com>
2020-12-06 06:29:55 -0800

    Day six, python, part 2

--- day-six/python/day_six.py
+++ day-six/python/day_six.py
@@ -5,6 +5,12 @@ if __name__ == "__main__":
 
     with open(filename) as inf:
         groups = inf.read().split("\n\n")
-        responses_by_group = [set(g for g in group if g != '\n') for group in groups]
+        responses_by_group = [g.split("\n") for g in groups]
 
-        print(f"Total: {sum(map(len, responses_by_group))}")
+        any_yes_by_group = [len(set.union(*map(set, rg))) for rg in responses_by_group]
+        print(f"Each by Group: {sum(any_yes_by_group)}")
+
+        all_yes_by_group = [
+            len(set.intersection(*map(set, rg))) for rg in responses_by_group
+        ]
+        print(f"Every by Group: {sum(all_yes_by_group)}")
