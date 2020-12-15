556287fd70b4e9e3cddc6ec14bb296c5bda9b1f0
Devon Terrell <devon@98point6.com>
2020-12-14 22:30:14 -0800

    Day fifteen, minor optimization

--- day-fifteen/python/day-fifeen.py
+++ day-fifteen/python/day-fifeen.py
@@ -4,28 +4,28 @@ import json
 import sys
 
 def bootstrap_game(seq):
-    memory = defaultdict(lambda: [])
+    memory = defaultdict(lambda: [None, None])
     last = None
     turn = 0
     for s in seq:
         s = int(s)
         turn += 1
         last = s
-        memory[s].append(turn)
-        # print(s)
+        _, later = memory[s]
+        memory[s][0] = later
+        memory[s][1] = turn
     return turn, last, memory
 
 def play_until_turn(turn, last, memory, target_turn):
     while turn < target_turn:
         turn += 1
-        if len(memory[last]) == 1:
+        if memory[last][0] is None:
             current = 0
         else:
-            current = memory[last][-1] - memory[last][-2]
-        memory[current].append(turn)
+            current = memory[last][1] - memory[last][0]
+        memory[current][0] = memory[current][1]
+        memory[current][1] = turn
         last = current
-        memory[current] = memory[current][-2:]
-        # print(current)
     return last, memory
 
 
@@ -39,5 +39,3 @@ if __name__ == "__main__":
     turn, last, memory = bootstrap_game(seq)
     last, memory = play_until_turn(turn, last, memory, nth_number)
     print(last)
-    # with open("output.json", "w") as outf:
-    #     json.dump(memory, outf)
