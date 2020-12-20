b0cd409c9185595c6b741c3666b94399990ad22a
Devon Terrell <devon@98point6.com>
2020-12-20 00:23:39 -0800

    Day nineteen part two HATE IT

--- day-nineteen/python/day_nineteen.py
+++ day-nineteen/python/day_nineteen.py
@@ -1,3 +1,4 @@
+from itertools import product
 import re
 import sys
 
@@ -29,6 +30,17 @@ def pattern_to_regex(literal_patterns, variable_pattern):
     return "".join(literal_patterns[c] for c in variable_pattern)
 
 
+def pattern_to_literal(literal_patterns, variable_pattern):
+    alternates = []
+    for c in variable_pattern:
+        subpattern = literal_patterns[c]
+        if not isinstance(subpattern, list):
+            alternates.append([subpattern])
+        else:
+            alternates.append(subpattern)
+    return [''.join(p) for p in list(product(*alternates))]
+
+
 def subpatterns_to_regex(literal_patterns, subpatterns):
     compiled_subpatterns = []
     for subpattern in subpatterns:
@@ -38,10 +50,18 @@ def subpatterns_to_regex(literal_patterns, subpatterns):
     return full_pattern
 
 
-def patterns_to_regex(literal_patterns, variable_patterns):
+def subpatterns_to_literals(literal_patterns, subpatterns):
+    compiled_subpatterns = []
+    for subpattern in subpatterns:
+        compiled = pattern_to_literal(literal_patterns, subpattern)
+        compiled_subpatterns.extend(compiled)
+    return compiled_subpatterns
+
+
+def patterns_to_target(literal_patterns, variable_patterns, target):
     new_literal_patterns = {}
     for pattern_name, subpatterns in variable_patterns.items():
-        full_pattern = subpatterns_to_regex(literal_patterns, subpatterns)
+        full_pattern = target(literal_patterns, subpatterns)
         new_literal_patterns[pattern_name] = full_pattern
     return new_literal_patterns
 
@@ -58,27 +78,41 @@ def find_unsatisfied_patterns(literal_patterns, rules):
     return {k: v for k, v in rules.items() if k not in literal_patterns}
 
 
-def compile_variable_patterns(literal_patterns, rules):
+def compile_variable_patterns(literal_patterns, rules, compilation_target):
     if not len(rules):
         return literal_patterns, None
     satisfied_patterns = find_satisfied_patterns(literal_patterns, rules)
     if len(satisfied_patterns) == 0:
         return literal_patterns, rules
-    new_literal_patterns = patterns_to_regex(literal_patterns, satisfied_patterns)
+    new_literal_patterns = patterns_to_target(literal_patterns, satisfied_patterns, compilation_target)
     unsatisfied_patterns = find_unsatisfied_patterns(new_literal_patterns, rules)
     return compile_variable_patterns(
-        {**literal_patterns, **new_literal_patterns}, unsatisfied_patterns
+        {**literal_patterns, **new_literal_patterns}, unsatisfied_patterns, compilation_target
     )
 
 
-def compile_patterns(rules):
+def compile_patterns(rules, compilation_target=subpatterns_to_regex):
     literal_patterns = {
         k: v[0][0][1]
         for k, v in rules.items()
         if len(v) == 1 and v[0][0].startswith('"')
     }
     other_rules = {k: v for k, v in rules.items() if k not in literal_patterns}
-    return compile_variable_patterns(literal_patterns, other_rules)
+    return compile_variable_patterns(literal_patterns, other_rules, compilation_target)
+
+
+def count_suffix(patterns, message, count = 0):
+    ends_with_any = any(message.endswith(p) for p in patterns)
+    if not ends_with_any:
+        return count
+    return count_suffix(patterns, message[:-1 * len(patterns[0])], count + 1)
+
+
+def count_prefix(patterns, message, count = 0):
+    starts_with_any = any(message.startswith(p) for p in patterns)
+    if not starts_with_any:
+        return count
+    return count_prefix(patterns, message[len(patterns[0]):], count + 1)
 
 
 if __name__ == "__main__":
@@ -91,3 +125,12 @@ if __name__ == "__main__":
     print(
         f"Matching Rule 0: {sum(1 for message in messages if re.fullmatch(compiled_patterns['0'], message))}"
     )
+
+    rule_updates, _ = parse_input(RULE_REPLACEMENTS)
+    updated_rules = {**rules, **rule_updates}
+    updated_compiled_patterns = compile_patterns(updated_rules, subpatterns_to_literals)[0]
+
+    prefix_counts = [count_prefix(updated_compiled_patterns["42"], m) for m in messages]
+    suffix_counts = [count_suffix(updated_compiled_patterns["31"], m) for m in messages]
+    print(sum(1 for i,p in enumerate(prefix_counts) if p > suffix_counts[i] and p != 0 and suffix_counts[i] != 0))
+    
